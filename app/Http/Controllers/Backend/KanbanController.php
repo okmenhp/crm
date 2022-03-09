92708aa525048425a48c0810e14dce18bccdf56a
dat <xibalaxopxep999999@gmail.com>
2022-03-09 17:13:00 +0700

    Dat

--- app/Http/Controllers/Backend/KanbanController.php
+++ app/Http/Controllers/Backend/KanbanController.php
@@ -6,17 +6,20 @@ namespace App\Http\Controllers\Backend;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 use App\Repositories\ListRepository;
-
+use App\Repositories\UserRepository;
 
 class KanbanController extends BaseController
 {
-	public function __construct(ListRepository $listRepo) {
+	public function __construct(ListRepository $listRepo, UserRepository $userRepo) {
         $this->listRepo = $listRepo;
+        $this->userRepo = $userRepo;
     }
 
     public function index($project_id)
     {
         $records = $this->listRepo->all()->where('project_id',$project_id);
-        return view('backend/kanban/index', compact('records','project_id'));
+        $user_option = $this->userRepo->all();
+        $user_html = \App\Helpers\StringHelper::getSelectFullNameOptions($user_option);
+        return view('backend/kanban/index', compact('records','project_id','user_html'));
     }
 }
