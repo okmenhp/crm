5706d93da1882179f3bc636918e44352f4c7ddda
dat <xibalaxopxep999999@gmail.com>
2022-03-17 14:08:10 +0700

    dat

--- database/migrations/2022_03_17_095848_add_status_and_ordering_to_list_table.php
+++ database/migrations/2022_03_17_095848_add_status_and_ordering_to_list_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RemoveUserIdTask extends Migration
+class AddStatusAndOrderingToListTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,10 @@ class RemoveUserIdTask extends Migration
      */
     public function up()
     {
-        Schema::table('task', function($table) {
-            $table->dropColumn('user_id');
-         });
+        Schema::table('list', function (Blueprint $table) {
+            $table->integer('ordering')->nullable();
+            $table->integer('status')->nullable();
+        });
     }
 
     /**
@@ -25,6 +26,8 @@ class RemoveUserIdTask extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('list', function (Blueprint $table) {
+            //
+        });
     }
 }
